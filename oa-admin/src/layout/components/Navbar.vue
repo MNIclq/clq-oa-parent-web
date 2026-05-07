85e9d9392d6b1639b961a68a2044c8853c28a5c0
clq <19838089421@163.com>
2026-05-07 15:05:00 +0800

    完善页面，处理页面bug

--- oa-admin/src/layout/components/Navbar.vue
+++ oa-admin/src/layout/components/Navbar.vue
@@ -16,9 +16,9 @@
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+          <!-- <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
             <el-dropdown-item>Github</el-dropdown-item>
-          </a>
+          </a> -->
           <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
             <el-dropdown-item>Docs</el-dropdown-item>
           </a>
